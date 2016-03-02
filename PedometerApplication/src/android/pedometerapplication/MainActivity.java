5cf65d7a14ddcb420ecc585514fbfd2991c5ed67
Samudraneel Bhattacharyya <s24bhatt@uwaterloo.ca>
2016-03-02 00:55:36 -0500

    complete main

--- PedometerApplication/src/android/pedometerapplication/MainActivity.java
+++ PedometerApplication/src/android/pedometerapplication/MainActivity.java
@@ -1,3 +1,32 @@
+/*
+ * Simple Pedometer Version 1.0.0
+ * 
+ * Wednesday, March 2nd, 2016.
+ * 
+ * Developers: Samudraneel Bhattacharyya, Cheng-Tung Wu.
+ * 
+ * Contributors: Isaac Sy, Brian Le.
+ * 
+ * APPLICATION DESCRIPTION
+ * 
+ * A simple free, easy to use Pedometer application.
+ * 
+ * The pedometer keeps track of your steps and displays it to the user. It also calculates the total distance the user has walked
+ * and the total calories the user has burned.
+ * 
+ * It is very easy to use! Simply start walking as you would with your phone in the pocket (or anywhere!) as soon as you turn the 
+ * pedometer on.  * This must be done because the pedometer will begin calibration immediately upon activation. The calibration will 
+ * last for approximately 3 seconds after which it will begin counting the users steps.
+ * 
+ * 
+ */
+
+
+
+
+
+
+
 package android.pedometerapplication;
 
 import java.util.ArrayList;
@@ -21,27 +50,64 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	static float lowPassOut_Z;
-	static float lowPassOut_Y;
-	static float lowPassOut_X;
-	static float magnitude;
-	static Button refresh; // reference to the refresh button
-	static Button pause_btn;
-	static Button resume_btn;
+	
+	
+	//----------------------------INITIALIZING ALL VARIABLES, BUTTON, TEXTVIEWS AND DATA STRUCTURES -------------------------------//
+	
+	//---------------------------VARIABLES--------------------------//
+	
+	static float lowPassOut_Z; // low-pass filter output from the z axis
+	static float lowPassOut_Y; // low-pass filter output from the y axis
+	static float lowPassOut_X; // low-pass filter output from the x axis
+	static float magnitude; // magnitude of the xyz values
 	static boolean pauseboo = false;
 	static float[] oldData = { 0.00f, 0.00f, 0.00f };
 	static int state = 0; // stores current state of step
 	static int steps = 0; // records number of steps
-	static TextView stepstv; //textview displaying number of steps
-	static TextView temp; //TEMPORARY VARIABLE
-	static TextView temp1,temp2,temp3,temp4,temp5,temp6;
-	static ArrayList<Float> dataPoints = new ArrayList<Float>(); // initializes arraylist for data points
+	static float alpha = 0.762f; //average step length worldwide (lazy coding)
+	static float beta = 0.05f; //average calories lost per step;
 
-	//static TextView stateTV;
+	//---------------------------BUTTONS---------------------------//
 	
+	static Button refresh; // reference to the refresh button
+	static Button pause_btn;
+	static Button resume_btn;
+
+	//-------------------------TEXTVIEW----------------------------//
+	
+	static TextView stepstv; // textview displaying number of steps
+	static TextView distancetv; // textview displaying distance traveled
+	static TextView caloriesBurnedtv; // textview displaying calories burned
+	static TextView temp, temp1, temp2, temp3, temp4, temp5, temp6; //TEMPORARY VARIABLES
+	
+	//-----------------------DATA STRUCTURES-----------------------//
+
+	static ArrayList<Float> dataPoints = new ArrayList<Float>(); // initializes arraylist for data points
 	static ArrayList<Float> calibration = new ArrayList<Float>(); // initializes arraylist for data points
-	static float threshold = 0.0f;
-	static float crossover = 0.0f;
+	static float threshold = 0.0f; //the calibrated average of the individual
+	static float crossover = 0.0f; //the calculated average after calibration
+	
+	//----------------------END OF VARIABLES------------------------------------------------------//
+
+	
+
+	//------------------------------------METHODS USED FOR APPLICATION---------------------------------//
+	
+	
+	// CALCULATES DISTANCE TRAVELED
+	static public float distance(int n) {
+		float temp;
+		temp = n*steps;
+		return temp;
+	}
+	
+	
+	// CALCULATES CALORIES BURNED
+	static public float calories (int k) {
+		float temp;
+		temp = k*beta;
+		return temp;
+	}
 	
 
 	//LOWPASS FILTER USED TO REDUCE NOISE FROM LINEAR ACCELEROMETER
@@ -84,6 +150,12 @@ public class MainActivity extends Activity {
 		
 		return magnitude;
 	}
+	
+	//--------------------------END OF METHODS---------------------------------------------//
+	
+	
+	
+	//------------------------PROGRAMMING INSIDE OF MAINACTIVITY STARTS HERE------------------------------------------//
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -118,6 +190,10 @@ public class MainActivity extends Activity {
 			
 			FrameLayout rl = (FrameLayout) rootView.findViewById(R.id.rl);
 
+			// temp variables are used to move other text down because we weren't
+			// too sure how to do it efficiently
+			
+			// this is very lazy programming
 			
 			temp = new TextView (rootView.getContext());
 			temp1 = new TextView (rootView.getContext());
@@ -128,14 +204,34 @@ public class MainActivity extends Activity {
 			temp6 = new TextView (rootView.getContext());
 			
 			
+			// Steps textview orientation, size, colour
 			stepstv = new TextView (rootView.getContext());
 			stepstv.setGravity(Gravity.CENTER);
 			stepstv.setTextSize(20);
-			stepstv.setTextColor(Color.WHITE);
+			stepstv.setTextColor(Color.BLACK);
+			
+			
+			// Distance textview orientation, size, colour
+			distancetv = new TextView (rootView.getContext());
+			distancetv.setGravity(Gravity.CENTER);
+			distancetv.setTextSize(15);
+			distancetv.setTextColor(Color.BLACK);
+			
+			
+			// Calories textview orientation, size, colour
+			caloriesBurnedtv = new TextView (rootView.getContext());
+			caloriesBurnedtv.setGravity(Gravity.CENTER);
+			caloriesBurnedtv.setTextSize(15);
+			caloriesBurnedtv.setTextColor(Color.BLACK);
+			
 
+			// Setting initial text on screen
+			stepstv.setText("Steps: 0");
+			distancetv.setText("Distance walked: 0 m");
+			caloriesBurnedtv.setText("Calories burned: 0.0 cal");
 
-			stepstv.setText("OVERRIDE");
-
+			
+			// Adding text on screen in order specified below
 			rl.addView(temp);
 			rl.addView(temp1);
 			rl.addView(temp2);
@@ -144,6 +240,11 @@ public class MainActivity extends Activity {
 			rl.addView(temp5);
 			rl.addView(temp6);
 			rl.addView(stepstv);
+			rl.addView(distancetv);
+			rl.addView(caloriesBurnedtv);
+			
+			
+			// Buttons being initialized and their functions being defined
 			
 			
 			refresh = (Button) rootView.findViewById(R.id.Refresh);
@@ -175,18 +276,15 @@ public class MainActivity extends Activity {
 					
 				}
 			});
+			
+			
+			// Sensor manager is being called for and Linear Acceleration sensor being selected
 
-			SensorManager sensorManager = (SensorManager) rootView.getContext().getSystemService(SENSOR_SERVICE); // request
-																													// sensor
-																													// manager
-			Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); // get
-																								// reference
-																								// to
-																								// acceleration
-																								// sensor
+			SensorManager sensorManager = (SensorManager) rootView.getContext().getSystemService(SENSOR_SERVICE); 
+			Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); 
 
 
-			SensorEventListener a = new AccelerationEventListener(stepstv);
+			SensorEventListener a = new AccelerationEventListener(stepstv, distancetv, caloriesBurnedtv);
 			sensorManager.registerListener(a, accSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
 			return rootView;
@@ -194,14 +292,21 @@ public class MainActivity extends Activity {
 	}
 	
 	
+	// Acceleration Event Listener (class that actually does all the tasks)
 
 	static public class AccelerationEventListener implements SensorEventListener {
 		
+		
+		// Initialize all the required TextView variables
 		TextView output_steps;
+		TextView output_distance;
+		TextView output_caloriesBurned;
 
-		public AccelerationEventListener(TextView output) {
+		public AccelerationEventListener(TextView output_count, TextView output_walked, TextView output_burned) {
 			
-			output_steps = output;
+			output_steps = output_count;
+			output_distance = output_walked;
+			output_caloriesBurned = output_burned;
 			
 		}
 
@@ -209,6 +314,8 @@ public class MainActivity extends Activity {
 		public void onSensorChanged(SensorEvent event) {
 			if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
 				
+				
+				// Low-pass filter is used here to reduce environmental noise
 				oldData = event.values;
 				lowpass(oldData, event.values);
 
@@ -217,7 +324,11 @@ public class MainActivity extends Activity {
 			magnitude_of_xyz(lowPassOut_X, lowPassOut_Y, lowPassOut_Z);
 
 			
-			
+			// Calibration occurs here
+			// For future reference: IMPROVE THE CALIBRATION
+			// Potential ideas: REMOVE THE OLDEST INPUT WITH THE NEWEST INPUT
+			// Potential ideas: IMPLEMENT A NEURAL NETWORK
+			// Potential ideas: USE A GPS TO CALCULATE DISTANCE RATHER THAN AN APPROXIMATION
 			if (calibration.size() <= 100) {
 				calibration.add(magnitude);
 				threshold = averageData(calibration);
@@ -227,10 +338,11 @@ public class MainActivity extends Activity {
 			}
 			
 			
+			
+			// Pedometer finite state machine calculations occur in this else statement
 			else{
 				dataPoints.add(magnitude);
-
-				//temp.setText("Magnitude is: "+ magnitude);
+				
 				temp.setText("");
 
 				if (dataPoints.size() > 4) {
@@ -249,8 +361,6 @@ public class MainActivity extends Activity {
 					case 1: {
 						if (crossover > threshold) {
 							state = 2;
-							//steps++;
-							//output_steps.setText("Steps: " + String.valueOf(steps));
 						}
 						break;
 					}
@@ -264,6 +374,8 @@ public class MainActivity extends Activity {
 							state = 0;
 							steps++;
 							output_steps.setText("Steps: " + String.valueOf(steps));
+							output_distance.setText("Distance walked: " + String.format("%.3g%n", distance(steps)) + " m");
+							output_caloriesBurned.setText("Calories burned: " + String.format("%.1g%n", calories(steps)) + " cal");
 						}
 						break;
 					}
